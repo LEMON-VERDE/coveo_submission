1a9853a0dd0d8e7c5f0a15f14b3e6b0e1b06cfda
MoronixProduct3 <moronicproduct3@gmail.com>
2017-10-13 02:13:17 -0400

    added json response

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const parser = require('body-parser');
 const fs = require('fs');
+const solve = require('./enigme.js');
+
+
+const payload = JSON.parse(fs.readFileSync('./team.json'),'utf8');
 
 const app = express();
-
 app.use(parser.json());
-
 app.set('port', process.env.PORT || 80);
 
 app.post('', (req, res)=>{
-    
-    fs.writeFile('./request.log',JSON.stringify(req.body));
-    res.send('Express Works');
-    console.log('saved message');
+    payload.solution = solve(req.body.shapes);
+    res.json(payload);
 });
 
 app.listen(app.get('port'));
