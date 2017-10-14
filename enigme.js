2887da9341d233df6c5692ac50734ac9ffdba21a
MoronixProduct3 <moronicproduct3@gmail.com>
2017-10-14 16:21:28 -0400

    Added enigma solution

--- enigme.js
+++ enigme.js
@@ -1,5 +1,37 @@
 module.exports = function (shapes)
 {
+    if (!Array.isArray(shapes))
+        return 0;
 
-    return 0;
+    var nbOfShapes = 0;
+
+    for (let straws of shapes) {
+        if (shapeIsValid(straws))
+            ++nbOfShapes;
+    }
+
+    return nbOfShapes;
+}
+
+function shapeIsValid(straws){
+    if(!Array.isArray(straws) || straws.length < 3)
+        return false;
+
+    let longest = 0;
+    let sum = 0;
+
+    for (let currentStraw of straws) {
+        if (currentStraw > longest) {
+            sum += longest;
+            longest = currentStraw;
+        }
+        else {
+            sum += currentStraw;
+        }
+    }
+
+    if (longest > sum)
+        return false;
+    else
+        return true;
 }
